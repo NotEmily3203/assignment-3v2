6b1bb3161c07e7eac2e093941930feb8e2b8f7d4
NotEmily3203 <linemily62@gmail.com>
2024-04-07 17:12:46 -0400

    i think this works

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,33 +2,12 @@ import React, {useState, useEffect} from 'react';
 
 import AccountBalance from './AccountBalance';
 const Home = ({balance, credits, debits, updateBalance}) => {
-    const [tempBalance, setTempBalance] = useState(0);
-    useEffect(() => {
-        let totalBalance = 0;
     
-        if (credits && credits.length !== 0) {
-            for(let i =0; i<credits.length; i++){
-                totalBalance += credits[i].amount;
-            }
-        }
-    
-        if (debits && debits.length !== 0) {
-            for(let i =0; i<debits.length; i++){
-                totalBalance -= debits[i].amount;
-            }
-        }
-    
-        setTempBalance(totalBalance);
-      }, [credits, debits]);
-    
-      useEffect(() => {
-        updateBalance(tempBalance);
-      }, [tempBalance]);
     return(
         <div>
             <img src="https://picsum.photos/200/200" alt="bank"/>
             <h1>Bank of React</h1>
-
+            <AccountBalance balance={balance} credits={credits} debits={debits} updateBalance={updateBalance}/>
         </div>
     )
 }
